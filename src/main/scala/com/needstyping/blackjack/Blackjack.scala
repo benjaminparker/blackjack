a719d6950e325cbede1ed0d88ef60c3621d202fa
Nick Choudhry <nickchoudhry@Users-MacBook-Pro.local>
2018-06-13 11:25:03 +0100

    B01 - Is it BlackJack

--- src/main/scala/com/needstyping/blackjack/Blackjack.scala
+++ src/main/scala/com/needstyping/blackjack/Blackjack.scala
@@ -1,5 +1,10 @@
 package com.needstyping.blackjack
 
 trait Blackjack {
-  def isBlackjack(firstCard: Card, secondCard: Card): Boolean = (firstCard.value == Ace || secondCard.value == Ace) && ((firstCard.value == King || secondCard.value == King) || (firstCard.value == Queen || secondCard.value == Queen) || (firstCard.value == Jack || secondCard.value == Jack))
+  def isBlackjack(firstCard: Card, secondCard: Card): Boolean = {
+    if ((firstCard.value == Ace || secondCard.value == Ace) && (firstCard.value == King || secondCard.value == King)) true
+    else if ((firstCard.value == Ace || secondCard.value == Ace) && (firstCard.value == Queen || secondCard.value == Queen)) true
+    else if ((firstCard.value == Ace || secondCard.value == Ace) && (firstCard.value == Jack || secondCard.value == Jack)) true
+    else false
+  }
 }
