9f82d23dbf6255df200cd08579fe1e47d54383d9
Benjamin Parker <benjamin@needstyping.com>
2018-06-15 09:02:36 +0100

    Refactor to make simpler case / match

--- src/main/scala/com/needstyping/blackjack/Blackjack.scala
+++ src/main/scala/com/needstyping/blackjack/Blackjack.scala
@@ -1,21 +1,10 @@
 package com.needstyping.blackjack
 
 trait Blackjack {
-
-//  def isBlackjack(firstCard: Card, secondCard: Card): Boolean = {
-//    if ((firstCard.value == Ace || secondCard.value == Ace) && (firstCard.value == King || secondCard.value == King)) true
-//    else if ((firstCard.value == Ace || secondCard.value == Ace) && (firstCard.value == Queen || secondCard.value == Queen)) true
-//    else if ((firstCard.value == Ace || secondCard.value == Ace) && (firstCard.value == Jack || secondCard.value == Jack)) true
-//    else false
-//  }
-
-  //I wrote tests and they all pass everything works but I'm not sure this is what you meant.
-    def isBlackjack(firstCard: Card, secondCard: Card): Boolean = (firstCard, secondCard) match {
-      case (Card(Ace, _,false), Card(King, _,true)) => true
-      case (Card(King, _,true), Card(Ace, _,false)) => true
-      case (Card(Ace, _,false), Card(Ten, _,true)) => true
-      case (Card(Ten, _,true), Card(Ace, _,false)) => true
-      case _ => false
-    }
+  def isBlackjack(firstCard: Card, secondCard: Card): Boolean = (firstCard, secondCard) match {
+    case (Card(_, _, true), Card(Ace, _, _)) => true
+    case (Card(Ace, _, false), Card(_, _, true)) => true
+    case _ => false
+  }
 }
 
